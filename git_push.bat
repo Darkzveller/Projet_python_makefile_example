2ca366a0dc66a6936cd40cf0e2d7f895c9287c9a
Darkzveller <elkattoufi.youssef@gmail.com>
2026-09-24 22:13:37 +0200

    ajout doc

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "modfiication makefile par IA pour quelque chose de plus aboutie" && git push
+git status && git add . && git commit -m "ajout doc" && git push
